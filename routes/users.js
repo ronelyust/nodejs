da5da300c46b30d72a941f4ba97b2d4e10e6b99a
ronelyust <93993708+ronelyust@users.noreply.github.com>
2024-07-14 16:29:41 +0300

    Update users.js

--- routes/users.js
+++ routes/users.js
@@ -13,7 +13,7 @@ router.get('/:id', async (req, res) => {
     const id = req.params.id;
     try {
         // Waiting to find a user.
-        const user = await Users.findOne({ id: id});
+        const user = await Users.findOne({ id: id });
         if (user) {
             // If the user exists, send the formatted user as JSON response
             res.status(200).json(user);
